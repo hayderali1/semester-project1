c204b1f62f196a792ff62f3be11c602cf2161035
hayderali1 <haideralmamori94@gmail.com>
2023-11-13 17:43:50 +0300

    last working mobileESP32

--- mobile-esp32.ino
+++ mobile-esp32.ino
@@ -1,43 +1,33 @@
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
-#include <BLEAdvertising.h>
-
-class MyServerCallbacks : public BLEServerCallbacks {
-  void onConnect(BLEServer* pServer) {
-    Serial.println("Device connected");
-  }
-
-  void onDisconnect(BLEServer* pServer) {
-    Serial.println("Device disconnected");
-  }
-};
 
 #define DEVICE_NAME "MobileESP32"
 #define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 
-BLEAdvertising* pAdvertising;
+BLEServer* pServer;
+BLEService* pService;
 
 void setup() {
   Serial.begin(115200);
+
   BLEDevice::init(DEVICE_NAME);
 
-  BLEServer* pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
+  pServer = BLEDevice::createServer();
+  pService = pServer->createService(BLEUUID(SERVICE_UUID));
 
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-
-  pAdvertising = pServer->getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_UUID);
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(pService->getUUID());
   pAdvertising->setScanResponse(true);
-  pAdvertising->setMinPreferred(0x06);  // 6ms
-  pAdvertising->setMinPreferred(0x12);  // 12ms
 
   pService->start();
-  pAdvertising->start();
+
+  BLEDevice::startAdvertising();
+  Serial.println("Setting up BLE...");
   Serial.println("Waiting for connections...");
 }
 
 void loop() {
-  // You can perform other tasks here
+  // Your main program logic goes here
+  delay(1000);
 }
